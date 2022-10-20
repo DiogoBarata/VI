f16622ac2d99a6458ec6a00d9c3b0c2859755699
DiogoBarata <diogo.barata@tecnico.ulisboa.pt>
2022-10-20 23:12:37 +0100

    Add Race and started working on filters

--- datasets/makeJSON.py
+++ datasets/makeJSON.py
@@ -30,14 +30,32 @@ import json
 
 CLASSES = ['Artificer','Barbarian','Bard','Cleric','Druid','Fighter','Monk','Paladin',
 'Ranger','Rogue','Sorcerer','Warlock','Wizard']
-ALIGNS=['CG','CN','LG','NG','LN','LE','CE','NN','NE']
+ALIGNS = ['CG','CN','LG','NG','LN','LE','CE','NN','NE']
+RACES = ['Aarakocra', 'Aasimar', 'Bugbear', 'Centaur', 'Changeling', 'Custom', 
+        'Dragonborn', 'Dwarf', 'Eladrin', 'Elf', 'Firbolg', 'Genasi', 'Gith', 'Gnome', 
+        'Goblin', 'Goliath', 'Half-Elf', 'Half-Orc', 'Halfling', 'Hobgoblin', 'Human', 
+        'Kalashtar', 'Kenku', 'Kobold', 'Leonin', 'Lizardfolk', 'Loxodon', 'Minotaur', 
+        'Orc', 'Satyr', 'Shifter', 'Simic hybrid', 'Tabaxi', 'Tiefling', 'Triton', 
+        'Turtle', 'Vedalken', 'Warforged', 'Yaun-Ti']
+
 VARS = ["class","alignment"]
 filename = "datasets/cleaned/dnd_chars_all_cleaned.json"
+
+#relations  |race   |class  |align  |
+# race      |   \   |   x   |   x   |
+# class     |   x   |   \   |   x   |
+# align     |   X   |   x   |   \   |
 
 class_align_count = {}
 align_class_count = {}
 
+race_class_count = {}
+class_race_count = {}
 
+race_align_count = {}
+align_race_count = {}
+
+# Generate dicts to use for the JSON
 def class_align(data,char):
     aux_align = data[char]['alignment'][0]
     for char_class in data[char]['class']:
@@ -64,32 +82,72 @@ def align_class(data,char):
                     else:
                         align_class_count[aux_align][char_class] += 1
 
+def class_race(data,char):
+    aux_race = data[char]['race'][0]
+    for char_class in data[char]['class']:
+        if char_class in CLASSES:
+            if aux_race != '':
+                if char_class not in class_race_count:
+                    class_race_count[char_class] = {aux_race:1}
+                else:
+                    if aux_race not in class_race_count[char_class]:
+                        class_race_count[char_class][aux_race] = 1
+                    else:
+                        class_race_count[char_class][aux_race] += 1
 
-def class_align_json(one_class):
-    relation = list(class_align_count[one_class].keys())
+def race_class(data,char):
+    aux_race = data[char]['race'][0]
+    if aux_race != '':
+        for char_class in data[char]['class']:
+            if char_class in CLASSES:
+                if aux_race not in race_class_count:
+                    race_class_count[aux_race] = {char_class:1}
+                else:
+                    if char_class not in race_class_count[aux_race]:
+                        race_class_count[aux_race][char_class] = 1
+                    else:
+                        race_class_count[aux_race][char_class] += 1
+
+def race_align(data,char):
+    aux_race = data[char]['race'][0]
+    aux_align = data[char]['alignment'][0]
+    if aux_race != '':
+        if aux_align != 'None':
+            if aux_race not in race_align_count:
+                race_align_count[aux_race] = {aux_align:1}
+            else:
+                if aux_align not in race_align_count[aux_race]:
+                    race_align_count[aux_race][aux_align] = 1
+                else:
+                    race_align_count[aux_race][aux_align] += 1
+
+def align_race(data,char):
+    aux_race = data[char]['race'][0]
+    aux_align = data[char]['alignment'][0]
+    if aux_race != '':
+        if aux_align != 'None':
+            if aux_align not in align_race_count:
+                align_race_count[aux_align] = {aux_race:1}
+            else:
+                if aux_race not in align_race_count[aux_align]:
+                    align_race_count[aux_align][aux_race] = 1
+                else:
+                    align_race_count[aux_align][aux_race] += 1
+
+# Generate JSONS structures
+def create_json(center,count_dict):
+    relation = list(count_dict[center].keys())
     id = 1
     network_json = {'nodes':[],'links':[]}
-    network_json['nodes'].append({'id':id,'name':one_class})
+    network_json['nodes'].append({'id':id,'name':center})
     id +=1
     for rel in relation:
         network_json['nodes'].append({'id':id,'name':rel})
-        network_json['links'].append({'source':1,'target':id,'distance':class_align_count[one_class][rel]})
+        network_json['links'].append({'source':1,'target':id,'distance':count_dict[center][rel]})
         id+=1
     return network_json
 
-def align_class_json(one_align):
-    relation = list(align_class_count[one_align].keys())
-    id = 1
-    network_json = {'nodes':[],'links':[]}
-    network_json['nodes'].append({'id':id,'name':one_align})
-    id +=1
-    for rel in relation:
-        network_json['nodes'].append({'id':id,'name':rel})
-        network_json['links'].append({'source':1,'target':id,'distance':align_class_count[one_align][rel]})
-        id+=1
-    return network_json
-
-
+#----------Main------------
 with open(filename,"r",encoding="utf-8") as f:
     data = json.load(f)
     new_json = {'Class_Align':None}
@@ -97,18 +155,35 @@ with open(filename,"r",encoding="utf-8") as f:
     for character in data:
         class_align(data,character)
         align_class(data,character)
-    
-    tmp_json = {}
+        race_class(data,character)
+        class_race(data,character)
+        race_align(data,character)
+        align_race(data,character)
+    # Create and append the network JSON structure to a JSON file
+    class_align_json = {}
+    class_race_json = {}
     for fun_class in CLASSES:
-        tmp_json[fun_class] = class_align_json(fun_class)
-    new_json['Class_Align'] = tmp_json
+        class_align_json[fun_class] = create_json(fun_class,class_align_count)
+        class_race_json[fun_class] = create_json(fun_class,class_race_count)
+    new_json['Class_Align'] = class_align_json
+    new_json['Class_Race'] = class_race_json
+    
+    race_class_json = {}
+    race_align_json = {}
+    for fun_race in RACES:
+        race_class_json[fun_race] = create_json(fun_race, race_class_count)
+        race_align_json[fun_race] = create_json(fun_race, race_align_count)
+    new_json['Race_Class'] = race_class_json
+    new_json['Race_Align'] = race_align_json
 
-    tmp_json = {}
+    align_class_json = {}
+    align_race_json = {}
     for fun_align in ALIGNS:
-        tmp_json[fun_align] = align_class_json(fun_align)
-    new_json['Align_Class'] = tmp_json
-
+        align_class_json[fun_align] = create_json(fun_align,align_class_count)
+        align_race_json[fun_align] = create_json(fun_align,align_race_count)
+    new_json['Align_Class'] = align_class_json
+    new_json['Align_Race'] = align_race_json
 
 
 with open('network_all_data.json',"w") as f:
-    json.dump(new_json,f, indent=3)
+    json.dump(new_json,f,indent=2)
--- script.js
+++ script.js
@@ -1,31 +1,113 @@
+const margin = {top: 10, right: 30, bottom: 30, left: 40},
+      width = 500 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
+const options = {
+    "Classes": [
+        {"id": "root"},
+        {"id": "Classes", "parent":"root"},
+        {"id": "Artificer", "parent": "Classes"},
+        {"id": "Barbarian", "parent": "Classes"},
+        {"id": "Bard", "parent": "Classes"},
+        {"id": "Cleric", "parent": "Classes"},
+        {"id": "Druid", "parent": "Classes"},
+        {"id": "Fighter", "parent": "Classes"},
+        {"id": "Monk", "parent": "Classes"},
+        {"id": "Paladin", "parent": "Classes"},
+        {"id": "Ranger", "parent": "Classes"},
+        {"id": "Rogue", "parent": "Classes"},
+        {"id": "Sorcerer", "parent": "Classes"},
+        {"id": "Warlock", "parent": "Classes"},
+        {"id": "Wizard", "parent": "Classes"}
+    ],
+    "Races": [
+        {"id": "root"},
+        {"id": "Races", "parent":"root"},
+        {"id":"Aarakocra", "parent":"Races"},
+        {"id":"Aasimar", "parent":"Races"},
+        {"id":"Bugbear", "parent":"Races"},
+        {"id":"Centaur", "parent":"Races"},
+        {"id":"Changeling", "parent":"Races"},
+        {"id":"Custom", "parent":"Races"},
+        {"id":"Dragonborn", "parent":"Races"},
+        {"id":"Dwarf", "parent":"Races"},
+        {"id":"Eladrin", "parent":"Races"},
+        {"id":"Elf", "parent":"Races"},
+        {"id":"Firbolg", "parent":"Races"},
+        {"id":"Genasi", "parent":"Races"},
+        {"id":"Gith", "parent":"Races"},
+        {"id":"Gnome", "parent":"Races"},
+        {"id":"Goblin", "parent":"Races"},
+        {"id":"Goliath", "parent":"Races"},
+        {"id":"Half-Elf", "parent":"Races"},
+        {"id":"Half-Orc", "parent":"Races"},
+        {"id":"Halfling", "parent":"Races"},
+        {"id":"Hobgoblin", "parent":"Races"},
+        {"id":"Human", "parent":"Races"},
+        {"id":"Kalashtar", "parent":"Races"},
+        {"id":"Kenku", "parent":"Races"},
+        {"id":"Kobold", "parent":"Races"},
+        {"id":"Leonin", "parent":"Races"},
+        {"id":"Lizardfolk", "parent":"Races"},
+        {"id":"Loxodon", "parent":"Races"},
+        {"id":"Minotaur", "parent":"Races"},
+        {"id":"Orc", "parent":"Races"},
+        {"id":"Satyr", "parent":"Races"},
+        {"id":"Shifter", "parent":"Races"},
+        {"id":"Simic hybrid", "parent":"Races"},
+        {"id":"Tabaxi", "parent":"Races"},
+        {"id":"Tiefling", "parent":"Races"},
+        {"id":"Triton", "parent":"Races"},
+        {"id":"Turtle", "parent":"Races"},
+        {"id":"Vedalken", "parent":"Races"},
+        {"id":"Warforged", "parent":"Races"},
+        {"id":"Yaun-Ti", "parent":"Races"}
+    ]
+}
+const optionsNet = {
+    'NetFilter':[
+        {"id": "root"},
+        {"id": "Relation Node", "parent":"root"},
+        {"id": "Centre Node", "parent":"root"},
+        {"id": "Classes", "parent":"Relation Node"},
+        {"id": "Races", "parent":"Relation Node"},
+        {"id":"Alignment", "parent":"Relation Node"},
+        {"id": "Classes", "parent":"Centre Node"},
+        {"id": "Races", "parent":"Centre Node"},
+        {"id":"Alignment", "parent":"Centre Node"}
+    ]
+}
+
 function init(){
-    createVis3(".vis3");
+    createVis3(".vis3",'Align_Race','LG');
+    // someFilter('.class_filter','Classes',options);
+    // someFilter('.race_filter','Races',options);
+    // netFilter('.country_filter','NetFilter',optionsNet);
 }
 
 
-function createVis3(id){
-    margin = {top: 10, right: 30, bottom: 30, left: 40},
-    width = 500 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-
-    // append the svg object to the body of the page
+function createVis3(id,relation,centre){
+    // Append the svg object to the body of the page
     const svg = d3.select(id)
         .append("svg")
+        .attr("id","netVis")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
         .attr("class","node")
         .attr("transform",`translate(${margin.left}, ${margin.top})`);
+    
     d3.json("https://raw.githubusercontent.com/DiogoBarata/VI/main/network_all_data.json").then(function(data) {
+        const linkObject = data[relation][centre].links;
+        const nodeObject = data[relation][centre].nodes;
         // Initialize the links
         var link = svg
             .selectAll("line")
-            .data(data.Class_Align.Artificer.links)
+            .data(linkObject)
             .join("line")
             .style("stroke", "#aaa");
         // Initialize the nodes
         var node = svg.selectAll(".node")
-            .data(data.Class_Align.Artificer.nodes)
+            .data(nodeObject)
             .enter().append("g")
             .attr("class","node")
         node.append("circle")
@@ -36,19 +118,22 @@ function createVis3(id){
             .text(function(d){ return d.name;})
 
         // Initialize the network
-        const simulation = d3.forceSimulation(data.Class_Align.Artificer.nodes)         // Force algorithm is applied to nodes
-            .force("link", d3.forceLink()                                               // This force provides links between nodes
-            .distance(linkDistance)      
-            .id(function(d) { return d.id; })                                           // This provide  the id of a node
-            .links(data.Class_Align.Artificer.links)                                    // and this the list of links
+        const simulation = d3.forceSimulation(nodeObject)           // Force algorithm is applied to nodes
+            .force("link", d3.forceLink()                           // This force provides links between nodes
+                .distance(linkDistance)      
+                .id(function(d) { return d.id; })                   // This provide  the id of a node
+                .links(linkObject)                                  // and this the list of links
             )
-            .force("charge", d3.forceManyBody().strength(-1000))     					// This adds repulsion between nodes
-            .force("center", d3.forceCenter(width / 2, height / 2)) 					// This force attracts nodes to the center of the svg area
+            .force("charge", d3.forceManyBody().strength(-1000))    // This adds repulsion between nodes
+            .force("center", d3.forceCenter(width / 1.5, height / 1.5)) // This force attracts nodes to the center of the svg area
             .on("end", ticked);
         function linkDistance(d) {
-            return d.distance;
+            calcDist = 10*(1/d.distance) + 1 
+            console.log(calcDist, d.target)
+            return calcDist;
         }
-        // This function is run at each iteration of the force algorithm, updating the nodes position.
+        // This function is run at each iteration of the force algorithm
+        // Updating the nodes position.
         function ticked() {
             link
                 .attr("x1", d => d.source.x)
@@ -57,6 +142,163 @@ function createVis3(id){
                 .attr("y2", d => d.target.y);
             node
                 .attr("transform", d => `translate(${d.x},${d.y})`);
+            
         }
     });    
+}
+
+function updateNet(relation,centre){
+    d3.select("#netVis").remove();
+    createVis3(".vis3",relation,centre);
+}
+
+function someFilter(filterId,filterType,data){
+    // Node content
+	function renderNode(selection, rcd) {
+		selection.append('input')
+    		.attr('type', 'checkbox')
+    		.on('change', function () {
+      			d3.select('#selected')
+				.text(checkboxValues(d3.select(filterId)));
+    		});
+		selection.append('span')
+    		.text(rcd.id);
+	}
+	// Return array of ids that is checked
+	function checkboxValues(selection) {
+		console.log(selection.select('.body')
+			.selectAll('input:checked').data().map(d => d.id));
+		return selection.select('.body')
+			.selectAll('input:checked').data().map(d => d.id);
+	}
+	// Recursively append child nodes
+	function nextLevel(selection, node) {
+		const label = selection.append('span');
+		const arrow = label.append('span').classed('arrow', true);
+		label.call(renderNode, node.data);
+		if (!node.hasOwnProperty('children')) return;
+		const items = selection.append('ul')
+			.style('list-style-type', 'none')
+			.selectAll('li')
+			.data(node.children, d => d.id);
+		items.exit().remove();
+		items.enter()
+			.append('li').merge(items)
+			.each(function (d) {
+				d3.select(this).call(nextLevel, d);
+			});
+		label.select('.arrow')
+			.text('▼ ')
+			.on('click', function () {  // Collapse on click
+				const childList = selection.select('ul');
+				if (!childList.size()) return;
+				const expanded = childList.style('display') !== 'none';
+				d3.select(this).text(expanded ? '▶ ' : '▼ ');
+				childList.style('display', expanded ? 'none' : 'inherit');
+			});
+	}
+	// Generate tree view
+	function tree(selection) {
+		selection
+			.classed('viewport', true)
+			.style('overflow-y', 'scroll')
+			.style('height', '500px')
+			.append('div')
+			.classed('body', true)
+			.style('transform', 'scale(1.5)')
+			.style('transform-origin', 'top left');
+	}
+	// Update tree data
+	function updateTree(selection, items) {
+		const root = d3.stratify()
+			.id(d => d.id)
+			.parentId(d => d.parent)(items);
+		selection.select('.body')
+			.call(nextLevel, root);
+		// Remove dummy root node
+		selection.select('.body > span').remove();
+		selection.select('.body > ul').style('padding-left', 0);
+	}
+	
+	// Render
+	d3.select(filterId + ' div').remove();
+	d3.select(filterId).append('div')
+		.call(tree)
+		.call(updateTree, data[filterType]);
+
+}
+
+function netFilter(filterId,filterType,data){
+    // Node content
+	function renderNode(selection, rcd) {
+		selection.append('input')
+    		.attr('type', 'checkbox')
+    		.on('change', function () {
+      			d3.select('#selected')
+				.text(checkboxValues(d3.select(filterId)));
+    		});
+		selection.append('span')
+    		.text(rcd.id);
+	}
+	// Return array of ids that is checked
+	function checkboxValues(selection) {
+		var selectedNetFilters = (selection.select('.body')
+			.selectAll('input:checked').data().map(d => d.id));
+        console.log(selectedNetFilters)
+	}
+	// Recursively append child nodes
+	function nextLevel(selection, node) {
+		const label = selection.append('span');
+		const arrow = label.append('span').classed('arrow', true);
+		label.call(renderNode, node.data);
+		if (!node.hasOwnProperty('children')) return;
+		const items = selection.append('ul')
+			.style('list-style-type', 'none')
+			.selectAll('li')
+			.data(node.children, d => d.id);
+		items.exit().remove();
+		items.enter()
+			.append('li').merge(items)
+			.each(function (d) {
+				d3.select(this).call(nextLevel, d);
+			});
+		label.select('.arrow')
+			.text('▼ ')
+			.on('click', function () {  // Collapse on click
+				const childList = selection.select('ul');
+				if (!childList.size()) return;
+				const expanded = childList.style('display') !== 'none';
+				d3.select(this).text(expanded ? '▶ ' : '▼ ');
+				childList.style('display', expanded ? 'none' : 'inherit');
+			});
+	}
+	// Generate tree view
+	function tree(selection) {
+		selection
+			.classed('viewport', true)
+			.style('overflow-y', 'scroll')
+			.style('height', '500px')
+			.append('div')
+			.classed('body', true)
+			.style('transform', 'scale(1.5)')
+			.style('transform-origin', 'top left');
+	}
+	// Update tree data
+	function updateTree(selection, items) {
+		const root = d3.stratify()
+			.id(d => d.id)
+			.parentId(d => d.parent)(items);
+		selection.select('.body')
+			.call(nextLevel, root);
+		// Remove dummy root node
+		selection.select('.body > span').remove();
+		selection.select('.body > ul').style('padding-left', 0);
+	}
+	
+	// Render
+	d3.select(filterId + ' div').remove();
+	d3.select(filterId).append('div')
+		.call(tree)
+		.call(updateTree, data[filterType]);
+
 }
